2c02d7cabf13473af532fd5603f1f18391949304
Among-Rex <80576723+Among-Rex@users.noreply.github.com>
2021-07-15 14:50:35 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ block_image_height = 5;
 function load_img() {
 	fabric.Image.fromURL("golf-h.png", function(Img) {
 		hole_obj = Img;
-		hole_obj.scaleToWidth = (50);
-		hole_obj.scaleToHeight = (50);
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
 		hole_obj.set({
 			top:hole_y,
 			left:hole_x
@@ -25,8 +25,8 @@ function load_img() {
 function new_image() {
 	fabric.Image.fromURL("ball.png", function(Img) {
 		ball_obj = Img;
-		ball_obj.scaleToWidth = (50);
-		ball_obj.scaleToHeight = (50);
+		ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
 		ball_obj.set({
 			top:ball_y,
 			left:ball_x
